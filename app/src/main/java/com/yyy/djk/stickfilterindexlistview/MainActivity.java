247887290f864b8d65224ebcf6dc24b0992f8e8f
dongjunkun <1440418246@qq.com>
2015-07-07 17:23:26 +0800

    refactor code

--- app/src/main/java/com/yyy/djk/stickfilterindexlistview/MainActivity.java
+++ app/src/main/java/com/yyy/djk/stickfilterindexlistview/MainActivity.java
@@ -26,7 +26,7 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SideBar.OnTouchingLetterChangedListener, AdapterView.OnItemClickListener, TextWatcher {
 
     @InjectView(R.id.clearEditText) ClearEditText mClearEditText;
     @InjectView(R.id.stickListHeadersListView) StickyListHeadersListView mStickListHeadersListView;
@@ -57,45 +57,13 @@ public class MainActivity extends AppCompatActivity {
         characterParser = CharacterParser.getInstance();
         pinyinComparator = new PinyinComparator();
         mSideBar.setTextView(mDialog);
-        mSideBar.setOnTouchingLetterChangedListener(new SideBar.OnTouchingLetterChangedListener() {
-            @Override
-            public void onTouchingLetterChanged(String s) {
-                //该字母首次出现的位置
-                int position = cityAdapter.getPositionForSection(s.charAt(0));
-                if (position != -1) {
-                    mStickListHeadersListView.setSelection(position);
-                }
-            }
-        });
+        mSideBar.setOnTouchingLetterChangedListener(this);
         //mStickListHeadersListView的点击事件
-        mStickListHeadersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, ((City) cityAdapter.getItem(position)).getName(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        mStickListHeadersListView.setOnItemClickListener(this);
         cityAdapter = new CityAdapter(cities);
         mStickListHeadersListView.setAdapter(cityAdapter);
-        /**
-         * 输入框搜索过滤
-         */
-
-        mClearEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //当输入框里面的值为空，更新为原来的列表，否则为过滤数据列表
-                filterData(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
+        //输入框搜索过滤
+        mClearEditText.addTextChangedListener(this);
     }
 
     /**
@@ -120,6 +88,36 @@ public class MainActivity extends AppCompatActivity {
         //根据a~z进行排序
         Collections.sort(filterDataList, pinyinComparator);
         cityAdapter.updateList(filterDataList);
+    }
+
+    @Override
+    public void onTouchingLetterChanged(String s) {
+        //该字母首次出现的位置
+        int position = cityAdapter.getPositionForSection(s.charAt(0));
+        if (position != -1) {
+            mStickListHeadersListView.setSelection(position);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(MainActivity.this, ((City) cityAdapter.getItem(position)).getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        //当输入框里面的值为空，更新为原来的列表，否则为过滤数据列表
+        filterData(s.toString());
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
     }
 
     class MyAsyncTask extends AsyncTask<Object, Integer, String> {
